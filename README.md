8f36f5a00b26688b1b3edc3daa82d14cc2cf3d61
Sean Thimons <sxthimons@gmail.com>
2024-07-13 14:07:16 -0400

    Updated readme

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Requires R \> 4.4.1 and the packages listed in the `curation.R` script
 Lines 1-37 will help install packages + local functions needed.
 
 Suggested to have packages installed, the files downloaded and read into the environment, or cached into 'feather' files. The frac_raw object should be around \~2.4 GB in size (6.5 M rows).
+
+![](images/clipboard-1326477141.png)
--- curation.R
+++ curation.R
@@ -8,11 +8,13 @@
     'feather',
     'esquisse',
     'skimr',
-    'timetk',
+    'timetk'
     )
   
   new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
   if(length(new.packages)) install.packages(new.packages)
+  
+  rm(list.of.packages, new.packages)
   
   # Packages----
   library(here)
@@ -445,7 +447,11 @@ tx_sf <- tx %>%
   select(ingredient_common_name, year, latitude, longitude) %>% 
   sf::st_as_sf(., coords = c('longitude', 'latitude'), crs = 'EPSG:4269')
 
-mapview(tx_sf, zcol = c('ingredient_common_name', 'year'), burst = FALSE, legend = TRUE)
+mapview(tx_sf, zcol = c(
+  'ingredient_common_name'
+  #,'year'
+  ), burst = FALSE, legend = TRUE)
+
   
   
   
